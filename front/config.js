04f245c427875161aca046186e3257c53fed6475
maxidrom <prefixoid@gmail.com>
2022-10-20 11:51:18 +0200

    githubSvgView added. days differs with colors.

--- front/config.js
+++ front/config.js
@@ -1,3 +1,11 @@
 //var saveDateScript = "http://localhost:8080/savedate";
-var saveDateScript = "https://calendarapi.vercel.app/api/saveDate.js";
+const saveDateScript = "https://calendarapi.vercel.app/api/saveDate.js";
 var loadDateScript = "https://calendarapi.vercel.app/api/loadDates.js";
+
+//gitHubSvg properties
+var cellSize = 27;
+var cellStyleDefault = "fill:#EBEDF0; stroke:#DFE1E4";
+var cellStyleDefaultHighlited = "fill:#00ED00; stroke:#DFE1E4";
+var cellRounding = 5;
+var cellMarging = 6;
+
